77428434d6c683d376ee66881a59e27c5fb6154a
Jarosław Pleskot <43443813+jarosp1984@users.noreply.github.com>
2022-04-30 11:09:44 +0200

    comments and readme changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # synchronized_thread
-A small library for serially executing tasks on a separate thread, with abilty to wait for completion.
+A small library for serially executing tasks on a separate thread, with an abilty to wait for completion.
+
+Depends only on the standard library.
+
+Compilation tested in:
+- MS Visual C++ 2019
+- Apple clang 12.0.0
--- src/thread.h
+++ src/thread.h
@@ -6,8 +6,8 @@
 class CThread
 {
 protected:
-    std::atomic_bool    m_run;      ///< Thread functions can test this variable inside their loops.
-    std::thread         m_thread;
+    std::atomic_bool    m_run;      //!< Thread functions can test this variable inside their loops.
+    std::thread         m_thread;   //!< Std thread object
 
 public:
     template <class _Fn, class... _Args>
@@ -16,11 +16,13 @@ public:
         , m_thread( std::thread(std::forward<_Fn>(_Fx), this, std::forward<_Args>(_Ax)...) )
     {}
 
+    //! Stop the thread loop
     void Stop()
     {
         m_run = false;
     }
 
+    //! Join the thread
     void Join()
     {
         m_thread.join();
